a7e96948a53576c9b5c0207bc524f56dcff20e0f
rikibob <bobiar0804@gmail.com>
2025-07-11 10:44:29 +0300

    feat(ws): add ws module

---
+++ src/modules/ws/ws.gateway.ts
@@ -0,0 +1,18 @@
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { CommentEntity } from '../../orm/entities/comment.entity';
+
+@WebSocketGateway({
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  },
+})
+export class WSGateway {
+  @WebSocketServer()
+  server: Server;
+
+  emitEventToClients(eventName: string, data: CommentEntity) {
+    this.server.emit(eventName, data);
+  }
+}
